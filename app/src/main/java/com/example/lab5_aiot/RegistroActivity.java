ed157981fd7b1f71fbd7e66dd40d34c9fe991dd1
Sebastian Manrique <a2018609@pucp.edu.pe>
2023-06-11 22:00:17 -0500

    Registro completado

--- app/src/main/java/com/example/lab5_aiot/RegistroActivity.java
+++ app/src/main/java/com/example/lab5_aiot/RegistroActivity.java
@@ -3,12 +3,57 @@ package com.example.lab5_aiot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.lab5_aiot.databinding.ActivityRegistroBinding;
+import com.example.lab5_aiot.entitites.Usuario;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class RegistroActivity extends AppCompatActivity {
+
+    ActivityRegistroBinding binding;
+    DatabaseReference db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registro);
+        binding = ActivityRegistroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        db = FirebaseDatabase.getInstance().getReference();
+
+        binding.buttonRegistrar.setOnClickListener(view -> {
+
+            
+            //Validación de todos los campos:
+            if( TextUtils.isEmpty(binding.editNombre.getText().toString().trim()) ){
+                Toast.makeText(this, "Ingresar Nombre", Toast.LENGTH_SHORT).show();
+            } else if (TextUtils.isEmpty(binding.editTextCorreo.getText().toString().trim())) {
+                Toast.makeText(this, "Ingresar Correo", Toast.LENGTH_SHORT).show();
+            } else if (TextUtils.isEmpty(binding.editTextTextPassword.getText().toString().trim())) {
+                Toast.makeText(this, "Ingresar contraseña", Toast.LENGTH_SHORT).show();
+            } else if (TextUtils.isEmpty(binding.editTextNumero.getText().toString().trim())) {
+                Toast.makeText(this, "Ingresar Numero de telefono", Toast.LENGTH_SHORT).show();
+            }else{
+                //Agregar el usuario
+                Usuario usuario = new Usuario();
+                usuario.setNombre(binding.editNombre.getText().toString());
+                usuario.setCorreo(binding.editTextCorreo.getText().toString());
+                usuario.setContrasenha(binding.editTextTextPassword.getText().toString());
+                usuario.setNumero(binding.editTextNumero.getText().toString());
+
+                db.child("usuarios").push().setValue(usuario);
+                Toast.makeText(this, "Usuario Creado", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
+        binding.textInicio.setOnClickListener(view -> {
+            finish();
+        });
     }
 }
---
+++ app/src/main/java/com/example/lab5_aiot/entitites/Usuario.java
@@ -0,0 +1,40 @@
+package com.example.lab5_aiot.entitites;
+
+public class Usuario {
+    private String nombre;
+    private String correo;
+    private String contrasenha;
+    private String numero;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasenha() {
+        return contrasenha;
+    }
+
+    public void setContrasenha(String contrasenha) {
+        this.contrasenha = contrasenha;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+}
