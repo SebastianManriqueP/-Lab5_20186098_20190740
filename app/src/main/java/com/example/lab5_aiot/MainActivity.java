a24060767be1e51c895880f767b9127580ecc34f
Sebastian Manrique <a2018609@pucp.edu.pe>
2023-06-11 20:31:53 -0500

    Pantalla de login y Registro

--- app/src/main/java/com/example/lab5_aiot/MainActivity.java
+++ app/src/main/java/com/example/lab5_aiot/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.lab5_aiot;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+
+import com.example.lab5_aiot.databinding.ActivityMainBinding;
+import com.firebase.ui.auth.AuthMethodPickerLayout;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    FirebaseFirestore db;
+    FirebaseAuth auth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.buttonGoogle.setOnClickListener(view -> {
+
+            Intent intent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(Arrays.asList(
+                            new AuthUI.IdpConfig.GoogleBuilder().build()
+                    )).build();
+
+            signInLauncher.launch(intent);
+        });
+
+        binding.textRegistro.setOnClickListener(view -> {
+            Intent intent = new Intent(this, RegistroActivity.class);
+            startActivity(intent);
+        });
     }
+
+    ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK) {
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    Log.d("msg-test", "Firebase uid: " + user.getUid());
+                } else {
+                    Log.d("msg-test", "Canceló el Log-in");
+                }
+            }
+    );
 }
