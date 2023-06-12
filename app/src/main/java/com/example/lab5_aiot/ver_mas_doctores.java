3ec3268817a896658b5b59c9844a82991c8dcefc
niurkaa <niurka_30@outlook.es>
2023-06-12 17:32:50 -0500

    4,5,6:)

---
+++ app/src/main/java/com/example/lab5_aiot/ver_mas_doctores.java
@@ -0,0 +1,13 @@
+package com.example.lab5_aiot;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ver_mas_doctores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_mas_doctores);
+    }
+}
